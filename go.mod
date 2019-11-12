dbe9138da4e884f76b102aadd66a3e6f6db1a423
stdevHsequeda <henrysg88@gmail.com>
2019-11-11 20:45:44 -0500

    Added bot test.

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/stdevHsequeda/InversorTelegramBot
 
 go 1.12
+
+require (
+	github.com/go-telegram-bot-api/docs v0.0.0-20181109235159-b89f38c30182 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -2,16 +2,39 @@ package main
 
 import (
 	"fmt"
+	_ "github.com/go-telegram-bot-api/telegram-bot-api"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
 	port := os.Getenv("PORT")
-	http.HandleFunc("/", helloworld)
-	if err := http.ListenAndServe(":"+port, http.DefaultServeMux); err != nil {
+
+	bot, err := tgbotapi.NewBotAPI("931110470:AAHmRc3jqseVa8W5qTrgjueR6HhU0PIOuTI")
+	if err != nil {
 		fmt.Println(err)
 	}
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if info.LastErrorDate != 0 {
+		log.Printf("[Telegram callback failed]%s", info.LastErrorMessage)
+	}
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServe(":"+port, http.DefaultServeMux)
+
+	for update := range updates {
+		log.Printf("%+v\n", update)
+	}
+
+	http.HandleFunc("/", helloworld)
 }
 
 func helloworld(w http.ResponseWriter, r *http.Request) {
