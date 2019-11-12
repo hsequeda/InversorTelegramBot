dee5f92f3d8c247223c1908782f6c5f5caf6c417
stdevHsequeda <henrysg88@gmail.com>
2019-11-11 22:27:00 -0500

    Making test

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	port := os.Getenv("PORT")
 	bot, err := tgbotapi.NewBotAPI("931110470:AAHmRc3jqseVa8W5qTrgjueR6HhU0PIOuTI")
 	if err != nil {
 		fmt.Println(err)
@@ -15,7 +17,7 @@ func main() {
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("https://inversortelegrambot.herokuapp.com/"+bot.Token, "cert.pem"))
+	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert("https://inversortelegrambot.herokuapp.com/", "cert.pem"))
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -26,8 +28,8 @@ func main() {
 	if info.LastErrorDate != 0 {
 		log.Printf("[Telegram callback failed]%s", info.LastErrorMessage)
 	}
-	updates := bot.ListenForWebhook("/" + bot.Token)
-	go http.ListenAndServeTLS("0.0.0.0:8443", "cert.pem", "key.pem", nil)
+	updates := bot.ListenForWebhook("/")
+	go http.ListenAndServeTLS("0.0.0.0:"+port, "cert.pem", "key.pem", nil)
 
 	for update := range updates {
 		log.Printf("%+v\n", update)
