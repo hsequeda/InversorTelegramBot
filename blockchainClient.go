e53e136714033d724523f872d8c4dcf69cb9690f
stdevHsequeda <henrysg88@gmail.com>
2019-11-14 23:43:52 -0500

    Test invite link to generate refers.

--- blockchainClient.go
+++ blockchainClient.go
@@ -94,46 +94,16 @@ func handleDeposit(w http.ResponseWriter, r *http.Request) {
 }
 
 func showData(w http.ResponseWriter, r *http.Request) {
-	status := r.URL.Query().Get("status")
-	address := r.URL.Query().Get("addr")
-	value := r.URL.Query().Get("value")
-	txid := r.URL.Query().Get("txid")
-	switch status {
-	case "0":
-		u, err := GetUserByAddress(address)
-		if err != nil {
-			logrus.Error(err)
-		}
-		logrus.Info(
-			fmt.Sprintf("Se ha detectado una tranccion "+
-				"no confirmada por parte del usuario %s.\n"+
-				"Transaction ID:%s", u.Name, txid))
-		break
-	case "1":
-		u, err := GetUserByAddress(address)
-		if err != nil {
-			logrus.Error(err)
-		}
-		logrus.Info(
-			fmt.Sprintf("Se ha detectado una tranccion "+
-				"parcialmente confirmada por parte del usuario %s.\n"+
-				"Transaction ID:%s", u.Name, txid))
-		break
-	case "2":
-		u, err := GetUserByAddress(address)
-		if err != nil {
-			logrus.Error(err)
-		}
-		AddInvestToUser(value, u.Id)
 
-		msg := tgbotapi.NewMessage(channel_id,
-			fmt.Sprintf("Nueva inversion:\n "+
-				"%s ha invertido %s BTC!\n"+
-				"Transaction ID:\n"+
-				"%s", u.Name, value, txid))
-		if _, err := bot.Send(msg); err != nil {
-			logrus.Error(err)
-		}
-		break
-	}
+	status := r.URL.Query().Get("status")
+	logrus.Info("status: ", status)
+	address := r.URL.Query().Get("addr")
+	logrus.Info("address: ", address)
+	value := r.URL.Query().Get("value")
+	logrus.Info("value: ", value)
+
+	txid := r.URL.Query().Get("txid")
+	logrus.Info("txid: ", txid)
+
+	// logrus.Print(string(b))
 }
