dedfdf200846ed8d206554df759ac4c4b5d44999
stdevHsequeda <henrysg88@gmail.com>
2019-11-14 23:46:37 -0500

    Test invite link to generate refers.

--- blockchainClient.go
+++ blockchainClient.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
 	"github.com/sirupsen/logrus"
 	"net/http"
 )
@@ -53,44 +52,48 @@ func handleDeposit(w http.ResponseWriter, r *http.Request) {
 	address := r.URL.Query().Get("addr")
 	value := r.URL.Query().Get("value")
 	txid := r.URL.Query().Get("txid")
-	switch status {
-	case "0":
-		u, err := GetUserByAddress(address)
-		if err != nil {
-			logrus.Error(err)
-		}
-		logrus.Info(
-			fmt.Sprintf("Se ha detectado una tranccion "+
-				"no confirmada por parte del usuario %s.\n"+
-				"Transaction ID:%s", u.Name, txid))
-		break
-	case "1":
-		u, err := GetUserByAddress(address)
-		if err != nil {
-			logrus.Error(err)
-		}
-		logrus.Info(
-			fmt.Sprintf("Se ha detectado una tranccion "+
-				"parcialmente confirmada por parte del usuario %s.\n"+
-				"Transaction ID:%s", u.Name, txid))
-		break
-	case "2":
-		u, err := GetUserByAddress(address)
-		if err != nil {
-			logrus.Error(err)
-		}
-		AddInvestToUser(value, u.Id)
-
-		msg := tgbotapi.NewMessage(channel_id,
-			fmt.Sprintf("Nueva inversion:\n "+
-				"%s ha invertido %s BTC!\n"+
-				"Transaction ID:\n"+
-				"%s", u.Name, value, txid))
-		if _, err := bot.Send(msg); err != nil {
-			logrus.Error(err)
-		}
-		break
-	}
+	fmt.Println(status)
+	fmt.Println(address)
+	fmt.Println(value)
+	fmt.Println(txid)
+	// switch status {
+	// case "0":
+	// 	u, err := GetUserByAddress(address)
+	// 	if err != nil {
+	// 		logrus.Error(err)
+	// 	}
+	// 	logrus.Info(
+	// 		fmt.Sprintf("Se ha detectado una tranccion "+
+	// 			"no confirmada por parte del usuario %s.\n"+
+	// 			"Transaction ID:%s", u.Name, txid))
+	// 	break
+	// case "1":
+	// 	u, err := GetUserByAddress(address)
+	// 	if err != nil {
+	// 		logrus.Error(err)
+	// 	}
+	// 	logrus.Info(
+	// 		fmt.Sprintf("Se ha detectado una tranccion "+
+	// 			"parcialmente confirmada por parte del usuario %s.\n"+
+	// 			"Transaction ID:%s", u.Name, txid))
+	// 	break
+	// case "2":
+	// 	u, err := GetUserByAddress(address)
+	// 	if err != nil {
+	// 		logrus.Error(err)
+	// 	}
+	// 	AddInvestToUser(value, u.Id)
+	//
+	// 	msg := tgbotapi.NewMessage(channel_id,
+	// 		fmt.Sprintf("Nueva inversion:\n "+
+	// 			"%s ha invertido %s BTC!\n"+
+	// 			"Transaction ID:\n"+
+	// 			"%s", u.Name, value, txid))
+	// 	if _, err := bot.Send(msg); err != nil {
+	// 		logrus.Error(err)
+	// 	}
+	// 	break
+	// }
 }
 
 func showData(w http.ResponseWriter, r *http.Request) {
